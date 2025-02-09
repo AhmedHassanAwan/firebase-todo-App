f8d8e47a20bd8160f9c98024b7a840308b0f521f
AhmedHasssanAwan <awanhasan2007@gmail.com>
2025-02-10 01:13:12 +0500

    1

--- config.js
+++ config.js
@@ -1,8 +1,7 @@
 
 
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.3.0/firebase-app.js";
-  import { getAuth } from "https://www.gstatic.com/firebasejs/11.3.0/firebase-auth.js";
-//   import { getfirebase } from "https://www.gstatic.com/firebasejs/11.3.0/firebase-firebase.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.3.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.3.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.3.0/firebase-firestore.js";
 
 
